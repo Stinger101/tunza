1e09b86934fb882c6a7bf72033b95aedd5fe136e
stinger101 <john.ngugi716@strathmore.edu>
2020-04-18 14:01:47 +0200

    fixed undefined variable error

--- app/Http/Controllers/ChildController.php
+++ app/Http/Controllers/ChildController.php
@@ -15,14 +15,14 @@ class ChildController extends Controller
     public function index()
     {
         //// TODO: allow for caregivers to view children
-        if(explode("/",$request->path())[1]=="caregiver"){
+        if(explode("/",request()->path())[1]=="caregiver"){
           return Child::whereHas("caregivers",function (Builder $query) {
               $query->where('user_id', \Auth::user()->id);
             })->get();
-        }else if(explode("/",$request->path())[1]=="user"){
+        }else if(explode("/",request()->path())[1]=="user"){
           return Child::where("parent_id",\Auth::user()->id)->get();
         }
-        
+
     }
 
     /**
