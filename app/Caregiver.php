0ca460833520ba556f559890b7174be2e41a45fb
stinger101 <john.ngugi716@strathmore.edu>
2020-04-18 13:20:02 +0200

    added caregiver-child relationship

--- app/Caregiver.php
+++ app/Caregiver.php
@@ -10,4 +10,8 @@ class Caregiver extends Model
 
     protected $fillable=["is_active","invited_on","status","status_changed_on","is_registered",
     "email_provided","user_id","parent_id","child_id","category_id"];
+
+    public function child(){
+      return $this->belongsTo("App\Child","child_id");
+    }
 }
