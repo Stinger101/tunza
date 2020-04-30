d4281b29c034c2e55afeec35342c228bdb9ee4e2
stinger101 <john.ngugi716@strathmore.edu>
2020-04-30 23:23:48 +0200

    bug fix mail

--- resources/views/caregiverinvite.blade.php
+++ resources/views/caregiverinvite.blade.php
@@ -1,9 +1,9 @@
 component('mail::message')
 # Invite to be a caregiver
 
-You have been invited on Tunza app by {{$user->name}} to be a caregiver for {{$invite->child->name}}. Please login to the application to accept the invite.
+You have been invited on Tunza app by test to be a caregiver for test. Please login to the application to accept the invite.
 
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' => 'http://127.0.0.1'])
 Download Tunza
 @endcomponent
 
