620d8c2cc9a0bcca85f977582a535fc5be66d086
stinger101 <john.ngugi716@strathmore.edu>
2020-05-01 20:59:57 +0200

    ficed include bug in callcontroller

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -42,7 +42,7 @@ class CallController extends Controller
           "receiver_id"=>$request->receiver_id,
           "call_type"=>$request->call_type
         ]);
-        broadcast(new CallMade($update))->toOthers();
+        broadcast(new \App\Events\CallMade($update))->toOthers();
         return $call;//user chould then use this to join prescense channel
     }
 
@@ -78,7 +78,7 @@ class CallController extends Controller
     public function update(Request $request, Call $call)
     {
         //
-        
+
     }
 
     /**
