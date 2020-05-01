e0cf56b2e8e0d967426d0f025a18bd25aca08f53
stinger101 <john.ngugi716@strathmore.edu>
2020-05-01 23:27:11 +0200

    semicolon missing in topic controller

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -100,7 +100,7 @@ class TopicController extends Controller
             $topic_id->topic=$request->topic;
           }
           if(isset($request->attachment)){
-            $attachment_url=$request->file('attachment_url')->storeAs('communication/topics',$topic_id->id)
+            $attachment_url=$request->file('attachment_url')->storeAs('communication/topics',$topic_id->id);
             $attachment_type=pathinfo("storage_path".$attachment_url,PATHINFO_EXTENSION);
             $topic_id->attachment_url=$attachment_url;
             $topic_id->attachment_type=$attachment_url;
