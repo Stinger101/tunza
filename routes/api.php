41cdb064f31c6b87d27d5941034022ec44c754cb
stinger101 <john.ngugi716@strathmore.edu>
2020-05-02 11:17:09 +0200

    changed broadcast routing

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:api')->post('/broadcast/auth',function (Request $request
     return new Response("Forbidden",404);
   }
   $pusher = new Pusher\Pusher(env('PUSHER_APP_KEY'),env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'),['cluster'=>'eu','useTLS'=>true]);
-  Log::info(env('PUSHER_APP_KEY'));
+  Log::info(config('broadcasting.connections.pusher.key'));
   Log::info(env('PUSHER_APP_SECRET'));
   Log::info(env('PUSHER_APP_ID'));
   $res=$pusher->socket_auth($request->channel_name,$request->socket_id);
