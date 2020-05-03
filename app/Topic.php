c86f33ecba53a0b48737d34e7dd48c3e461d9d39
stinger101 <john.ngugi716@strathmore.edu>
2020-05-03 11:53:45 +0200

    topics: editor relationship bug fix

--- app/Topic.php
+++ app/Topic.php
@@ -9,7 +9,7 @@ class Topic extends Model
     //
     protected $fillable=["topic","attachment_url","attachment_type","child_id","editor_id","visibility","status"];
     public function editor(){
-      return $this->hasOne("App\User","editor_id");
+      return $this->belongsTo("App\User","editor_id");
     }
     public function comments(){
       return $this->hasMany("App\Comment","topic_id");
